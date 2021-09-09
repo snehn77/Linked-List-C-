d6c40237cca3018ed70f1c24b7716e9a82057292
Sneh Nandu <snehn777@gmail.com>
2021-09-09 13:04:33 -0700

    Add files via upload

---
+++ MyLinkedList.cs
@@ -0,0 +1,294 @@
+﻿using System;
+
+
+namespace MyLinkedList
+{
+
+    public class MyLinkedList<T>
+    {
+        private class Node
+        {
+            public T value;
+            public Node next;
+        }
+
+        private Node Head;
+
+        public MyLinkedList()
+        {
+            Head = null;
+        }
+
+        public int Size()
+        {
+            int count = 0;
+            Node first = Head;
+
+            while (first != null)
+            {
+                count++;
+                first = first.next;
+            }
+            return count;
+        }
+
+        public void Insert(T value)
+        {
+            Node newNode = new Node();
+            newNode.value = value;
+            Console.ForegroundColor = ConsoleColor.Green;
+            if (Head == null)
+            {
+                Head = newNode;
+                Console.WriteLine("\nInserted Successfully!");
+            }
+            else
+            {
+                Node first = Head;
+                while (first.next != null)
+                {
+                    first = first.next;
+                }
+                first.next = newNode;
+                Console.WriteLine("\nInserted Successfully!");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void InsertAt(uint position, T value)
+        {
+            Node newNode = new Node();
+            newNode.value = value;
+
+            if (position == 1)
+            {
+                newNode.next = Head;
+                Head = newNode;
+            }
+            else if (position > Size() || position == 0)
+            {
+                Console.WriteLine("\nPosition Not in the list");
+                return;
+            }
+            else
+            {
+                Node first = Head;
+
+                for (int i = 1; i < position - 1; i++)
+                {
+                    first = first.next;
+                }
+                newNode.next = first.next;
+                first.next = newNode;
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nInserted Successfully!");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void Print()
+        {
+            var headOfList = Head;
+            try
+            {
+                while (headOfList.next != null)
+                {
+                    Console.Write(headOfList.value + " -> ");
+                    headOfList = headOfList.next;
+                }
+                Console.Write(headOfList.value + "\n");
+            }
+            catch (NullReferenceException)
+            {
+                Console.Write("Linked List is empty");
+            }
+        }
+
+        public void Delete(T value)
+        {
+            Node previous = null;
+            Node first = Head;
+            try
+            {
+                if (Head == null)
+                {
+                    Console.WriteLine("\nList is empty");
+                    return;
+                }
+
+                if (first != null && first.value.Equals(value))
+                {
+                    Head = first.next;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nDeleted Successfully!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    return;
+                }
+
+                while (first != null && !(first.value.Equals(value)))
+                {
+                    previous = first;
+                    first = first.next;
+                }
+                previous.next = first.next;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nDeleted Successfully!");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("\nElement not in the list");
+            }
+
+        }
+
+        public void DeleteAt(uint position)
+        {
+            int size = Size();
+            Node first = Head;
+            Node temp = null;
+
+            // If the given position is not in the linked List
+            if (position > size || position == 0)
+            {
+                Console.WriteLine("\nPosition is not in the Linked List");
+                return;
+            }
+            // Delete at head if position = 1
+            else if (position == 1)
+            {
+                Head = first.next;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nDeleted Successfully!");
+                Console.ForegroundColor = ConsoleColor.White;
+                return;
+            }
+            for (int i = 1; i < position - 1; i++)
+            {
+                first = first.next;
+            }
+            temp = first.next.next;
+            first.next.next = null;
+            first.next = temp;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nDeleted Successfully!");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void Center()
+        {
+            Node first = Head;
+            int size = Size();
+            int middle;
+            // Check if list is empty or not
+            if (first == null)
+            {
+                Console.WriteLine("\nList is Empty");
+            }
+            else
+            {
+                // If list has odd number of elements
+                if (size % 2 != 0)
+                {
+                    middle = size / 2;
+                    for (int i = 0; i < middle; i++)
+                    {
+                        first = first.next;
+                    }
+                    Console.WriteLine("\nValue at center: " + first.value);
+                }
+                // Else with list of even number of elements
+                else
+                {
+                    Console.WriteLine("\nList size is Even. Hence no center in the list");
+                }
+            }
+        }
+
+        public void Reverse()
+        {
+            Node previous = null;
+            Node current = Head;
+            Node temp = null;
+            // Checking if list is empty or not
+            if (current == null)
+            {
+                Console.WriteLine("\nList is Empty");
+                return;
+            }
+            // Reversing the list
+            while (current != null)
+            {
+                temp = current.next;
+                current.next = previous;
+                previous = current;
+                current = temp;
+            }
+
+            Head = previous;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nReversed Successful");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public void Iterator(MyLinkedList<T> myList)
+        {
+            if (Head == null)
+            {
+                Console.Write("\nLinked List is empty");
+                return;
+            }
+            MyLinkedList<T>.Enumerator enumerator = GetEnumerator();
+            enumerator.Reset();
+            Console.WriteLine("\nList using Iterator: ");
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+        }
+        public Enumerator GetEnumerator()
+        {
+            return new Enumerator(this);
+        }
+
+        public class Enumerator
+        {
+            private Node current;
+            private MyLinkedList<T> myList;
+
+            public Enumerator(MyLinkedList<T> list)
+            {
+                myList = list;
+                Reset();
+            }
+
+            public void Reset()
+            {
+                current = null;
+            }
+
+            public T Current
+            {
+                get
+                {
+                    return current.value;
+                }
+            }
+
+            public bool MoveNext()
+            {
+                if (current == null)
+                {
+                    current = myList.Head;
+                }
+                else
+                {
+                    current = current.next;
+                }
+                return (current != null);
+            }
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+namespace MyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLinkedList<int> myList = new MyLinkedList<int>();
+
+            int flag = 0, value;
+            uint position;
+        redo:
+            Console.Clear();
+            ShowOptions();
+            int choice = Choice();
+            while (flag == 0)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        value = GetValue();
+                        myList.Insert(value);
+                        PressToContinue();
+                        goto redo;
+
+                    case 2:
+                        position = GetPosition();
+                        value = GetValue();
+                        myList.InsertAt(position,value);
+                        PressToContinue();
+                        goto redo;
+
+                    case 3:
+                        value = GetValue();
+                        myList.Delete(value);
+                        PressToContinue();
+                        goto redo;
+
+                    case 4:
+                        position = GetPosition();
+                        myList.DeleteAt(position);
+                        PressToContinue();
+                        goto redo;
+
+                    case 5:
+                        myList.Center();
+                        PressToContinue();
+                        goto redo;
+
+                    case 6:
+                        myList.Reverse();
+                        PressToContinue();
+                        goto redo;
+
+                    case 7:
+                        Console.WriteLine("\nSize of List: " + myList.Size());
+                        PressToContinue();
+                        goto redo;
+
+                    case 8:
+                        myList.Iterator(myList);
+                        PressToContinue(); 
+                        goto redo;
+
+                    case 9:
+                        Console.Write("\nList: ");
+                        myList.Print();
+                        PressToContinue();
+                        goto redo;
+
+                    case 0:
+                        flag = 1;
+                        break;
+
+                    default:
+                        goto redo;
+                }
+            }
+
+        }
+
+        public static void ShowOptions()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Press 0 ---> Exit");
+            Console.WriteLine("Press 1 ---> Insert");
+            Console.WriteLine("Press 2 ---> Insert At Position");
+            Console.WriteLine("Press 3 ---> Delete");
+            Console.WriteLine("Press 4 ---> Delete At Position");
+            Console.WriteLine("Press 5 ---> Center");
+            Console.WriteLine("Press 6 ---> Reverse");
+            Console.WriteLine("Press 7 ---> Size");
+            Console.WriteLine("Press 8 ---> Iterator");
+            Console.WriteLine("Press 9 ---> Print");
+        }
+
+        public static int GetValue()
+        {
+            redo:
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("\nEnter Number To Insert/Delete: ");
+                int value = int.Parse(Console.ReadLine());
+                return value;
+            }
+            catch (FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nInvalid Type. Please enter a integer");
+                Console.ForegroundColor = ConsoleColor.White;
+                goto redo;
+            }
+        }
+
+        public static uint GetPosition()
+        {
+            Console.WriteLine("\nNote - Position Starts from index 1 i.e. To Insert/Delete at Head of Linked List press 1");
+            redo:
+            try
+            {
+                Console.Write("\nEnter index : ");
+                uint position = uint.Parse(Console.ReadLine());
+                return position;
+            }
+
+            catch (FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nInvalid Type. Please enter the type integer");
+                Console.ForegroundColor = ConsoleColor.White;
+                goto redo;
+            }
+            catch (OverflowException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nError: Position cannot be negative");
+                Console.ForegroundColor = ConsoleColor.White;
+                goto redo;
+            }
+        }
+
+        public static int Choice()
+        {
+            redo:
+            Console.Write("\nEnter Choice of Operation: ");
+            int choice;
+            try
+            {
+                choice = int.Parse(Console.ReadLine());
+                while (choice < 0 || choice > 9)
+                {
+                    Console.Write("\nPlease enter valid choice: ");
+                    choice = int.Parse(Console.ReadLine());
+                }
+                return choice;
+            }
+            catch (FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nError: Invalid Type! Please enter a integer from the given choices");
+                Console.ForegroundColor = ConsoleColor.White; 
+                goto redo;
+            }
+        }
+
+        public static void PressToContinue()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\n\nPress Any Key to continue: ");
+            Console.ReadKey();
+        }
+    }
+
+    
+}
